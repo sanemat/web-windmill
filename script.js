f8a6611b0e522aa25f7dead43e713c9a7943b4d3
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2019-12-22 13:37:35 +0700

    feat(audio): getMedia

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const constraints = { audio: true };
+
+async function getMedia(constraints) {
+  let stream = null;
+
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    /* use the stream */
+  } catch (err) {
+    /* handle the error */
+  }
+}
+
 document
   .getElementById("menu__setting_mic")
   .addEventListener("change", event => {
     if (event.target.checked) {
-      alert("checked");
+      getMedia(constraints);
     } else {
       alert("uncheched");
     }
